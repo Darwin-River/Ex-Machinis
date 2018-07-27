89c83b2a37b3edc02ee571c32398b01dfd02e408
freelancefrivas <freelance.frivas@gmail.com>
2018-07-27 18:42:02 -0300

    Updated mail templates with email domain

--- resources/views/email/welcome_text.blade.php
+++ resources/views/email/welcome_text.blade.php
@@ -10,7 +10,7 @@ All communications with these drones will be via email through the International
 Each of your drones and VDA’s are capable of receiving email instruction via a modified version of the FORTH programming language known as Venture FORTH (vFORTH).   In a series of forthcoming emails, we will introduce you to the basics of vFORTH and drone-based stellar industries.
 \r\n
 \r\n
-You can begin by requesting an asset summary from your VDA.  To do this simply send the vFORTH command “AssetReport” to dscc@exmaxinis.com.  This command should be contained in the body of the email and enclosed in by the XML &lt;script&gt; tag as follows:
+You can begin by requesting an asset summary from your VDA.  To do this simply send the vFORTH command “AssetReport” to dscc@{{ getenv("MAIL_HOST") }}.  This command should be contained in the body of the email and enclosed in by the XML &lt;script&gt; tag as follows:
 \r\n
 \r\n
 &lt;script&gt; \r\n
