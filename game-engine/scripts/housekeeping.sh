eead3499ed62bfe65632659e36e84506e80caf75
crodnun <crodnun@yahoo.es>
2019-05-26 18:48:39 +0200

    Clean actions & events entries older than 30 days, provisional script for housekeeping, pending to move it to C routine (too unsecure now)

--- game-engine/scripts/housekeeping.sh
+++ game-engine/scripts/housekeeping.sh
@@ -16,3 +16,11 @@ find $PLAT_HOME/log -mtime +7 -delete
 #find $PLAT_HOME/backup -mtime +1 -delete
 
 #mv ${BACKUP_FILE}.gz ${PLAT_HOME}/backup
+
+# Clean tables with old elements (provisional script, unsecure!!!)
+DB_PASSW=c20e5596eee9bf8519074ad62f51b0eaa5a415e805efa4b9
+DB_NAME=exmachinis
+DB_USER=root
+DB_HOST=localhost
+mysql -h ${DB_HOST} -u${DB_USER} -p${DB_PASSW} ${DB_NAME} -e "DELETE FROM actions WHERE timestamp < (NOW() - interval 30 day);"
+mysql -h ${DB_HOST} -u${DB_USER} -p${DB_PASSW} ${DB_NAME} -e "DELETE FROM events WHERE timestamp < (NOW() - interval 30 day);"
