f2ea0139c7e34a008ec9d772013af9654497b0d5
crodnun <crodnun@yahoo.es>
2019-08-07 20:38:26 +0200

    Removed code no longer required

--- game-engine/src/main_e.c
+++ game-engine/src/main_e.c
@@ -8,12 +8,6 @@
 
 /******************************* INCLUDES ************************************/
 
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <signal.h>
-//#include <sys/types.h>
-//#include <unistd.h>
-
 #include "engine_e.h"
 
 /******************************* DEFINES *************************************/
