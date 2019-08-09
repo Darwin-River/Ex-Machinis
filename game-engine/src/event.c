f05bbcd24235caa871a3b397873e89197a83e7b7
crodnun <crodnun@yahoo.es>
2019-08-09 18:04:35 +0200

    More logic to deal with events and outcome

--- game-engine/src/event.c
+++ game-engine/src/event.c
@@ -63,6 +63,12 @@ ErrorCode_t event_update_new_quantity(Event_t *event)
         event->new_quantity += previous_event.new_quantity;
     }
 
+    if(result == ENGINE_NOT_FOUND)
+    {
+        // not found is ok to continue logic
+        result = ENGINE_OK;
+    }
+
     return result;
 }
 
@@ -99,6 +105,12 @@ ErrorCode_t event_update_new_cargo(Event_t *event)
     {
         // We obtained and event - do the maths to update new_quantity
         event->new_cargo += previous_event.new_cargo;
+    }
+
+    if(result == ENGINE_NOT_FOUND)
+    {
+        // not found is ok to continue logic
+        result = ENGINE_OK;
     }
 
     return result;
@@ -140,7 +152,139 @@ ErrorCode_t event_update_new_credit(Event_t *event)
         event->new_credits += previous_event.new_credits;
     }
 
+    if(result == ENGINE_NOT_FOUND)
+    {
+        // not found is ok to continue logic
+        result = ENGINE_OK;
+    }
+
     return result;
+}
+
+/** ****************************************************************************
+
+  @brief      Check given condition is met
+
+  @param[in]  event  Input event
+
+  @return     Boolean (TRUE when condition is met, FALSE otherwise)
+
+*******************************************************************************/
+Bool_t event_no_local_condition(Event_t *event) 
+{
+    return ENGINE_FALSE;
+}
+
+/** ****************************************************************************
+
+  @brief      Check given condition is met
+
+  @param[in]  event  Input event
+
+  @return     Boolean (TRUE when condition is met, FALSE otherwise)
+
+*******************************************************************************/
+Bool_t event_no_resources_condition(Event_t *event) 
+{
+    return ENGINE_FALSE;
+}
+
+/** ****************************************************************************
+
+  @brief      Check given condition is met
+
+  @param[in]  event  Input event
+
+  @return     Boolean (TRUE when condition is met, FALSE otherwise)
+
+*******************************************************************************/
+Bool_t event_no_cargo_condition(Event_t *event) 
+{
+    int capacity = 0;
+
+    if(db_get_max_drone_cargo(event->drone_id, &capacity) == ENGINE_OK) {
+        // Compare current cargo vs. max capacity
+        engine_trace(TRACE_LEVEL_ALWAYS, 
+            "Current cargo [%d], max capacity [%d]", 
+            event->new_cargo, capacity);
+
+        if(capacity > event->new_cargo) {
+            return ENGINE_FALSE;
+        }
+        // else { we do not know the max capacity - reject event }
+    }
+
+    return ENGINE_TRUE;
+}
+
+/** ****************************************************************************
+
+  @brief      Check given condition is met
+
+  @param[in]  event  Input event
+
+  @return     Boolean (TRUE when condition is met, FALSE otherwise)
+
+*******************************************************************************/
+Bool_t event_no_credits_condition(Event_t *event) 
+{
+    return ENGINE_FALSE;
+}
+
+/** ****************************************************************************
+
+  @brief      Check given condition is met
+
+  @param[in]  event  Input event
+
+  @return     Boolean (TRUE when condition is met, FALSE otherwise)
+
+*******************************************************************************/
+Bool_t event_lower_resources_limit_condition(Event_t *event) 
+{
+    return ENGINE_FALSE;
+}
+
+/** ****************************************************************************
+
+  @brief      Check given condition is met
+
+  @param[in]  event  Input event
+
+  @return     Boolean (TRUE when condition is met, FALSE otherwise)
+
+*******************************************************************************/
+Bool_t event_upper_resources_limit_condition(Event_t *event) 
+{
+    return ENGINE_FALSE;
+}
+
+/** ****************************************************************************
+
+  @brief      Check given condition is met
+
+  @param[in]  event  Input event
+
+  @return     Boolean (TRUE when condition is met, FALSE otherwise)
+
+*******************************************************************************/
+Bool_t event_wrong_depletion_price_condition(Event_t *event) 
+{
+    return ENGINE_FALSE;
+}
+
+/** ****************************************************************************
+
+  @brief      Check given condition is met
+
+  @param[in]  event  Input event
+
+  @return     Boolean (TRUE when condition is met, FALSE otherwise)
+
+*******************************************************************************/
+Bool_t event_wrong_accumulation_price_condition(Event_t *event) 
+{
+    return ENGINE_FALSE;
 }
 
 /** ****************************************************************************
@@ -154,23 +298,51 @@ ErrorCode_t event_update_new_credit(Event_t *event)
 *******************************************************************************/
 ErrorCode_t event_validate_event_conditions(Event_t *event) 
 {
-  ErrorCode_t result = ENGINE_OK;
+    ErrorCode_t result = ENGINE_LOGIC_ERROR;
 
-  // TODO: Define an enum for the different outcome codes and use it at DB query!!!! now we use 0 directly !!!
+    // Update outcome value using Outcome_t enum depending on validations
 
-  // Check field by field
+    // Check event conditions to ensure that it can take place
+    if(event_no_local_condition(event) == ENGINE_TRUE) {
+        //a.  The local field is set to true but the affected drone is not local.
+        event->outcome = OUTCOME_NO_LOCAL;
+    } else if(event_no_resources_condition(event) == ENGINE_TRUE) {
+        //b.  The affected ship contains insufficient resources of the indicated state 
+        // (identical resource, locked, and installed values) to accommodate a depletion event. 
+        // This happens when the newly computed new_quantity field is less than zero.
+        event->outcome = OUTCOME_NO_RESOURCES;
+    } else if(event_no_cargo_condition(event) == ENGINE_TRUE) {
+        //c.  There affected ship contains insufficient cargo space to accommodate the accumulation event.
+        // This happens when the recently computed new_cargo value is greater than the cargo capacity of the drone (found in hull table)
+        event->outcome = OUTCOME_NO_CARGO_SPACE;
+    } else if(event_no_credits_condition(event) == ENGINE_TRUE) {
+        //d.  The player has insufficient credits to cover the purchase price of transferred items.
+        event->outcome = OUTCOME_NO_CREDITS;
+    } else if(event_lower_resources_limit_condition(event) == ENGINE_TRUE) {
+        // e.  The depletion event will violate the affected drone’s self imposed lower limit for the resource. 
+        // This only applies to cases in which the acting and affected ships are different.
+        event->outcome = OUTCOME_RESOURCES_LOWER_LIMIT;
+    } else if(event_upper_resources_limit_condition(event) == ENGINE_TRUE) {
+        //f.  The accumulation event will violate the affected drone’s self imposed upper limit for the resource. 
+        // This only applies to cases in which the acting and affected ships are different.        
+        event->outcome = OUTCOME_RESOURCES_UPPER_LIMIT;
+    } else if(event_wrong_depletion_price_condition(event) == ENGINE_TRUE) {
+        //g.  The Depletion Price for items being removed from the cargo bay of a non-allied ship is equal to or greater 
+        // than the price the player has set on those items.
+        event->outcome = OUTCOME_WRONG_DEPLETION_PRICE;
+    } else if(event_wrong_accumulation_price_condition(event) == ENGINE_TRUE) {
+        //h.  The Accumulation Price for items being added to the cargo bay of a non-allied ship is less than 
+        // or equal to the price the player has set for those items.
+        event->outcome = OUTCOME_WRONG_ACCUMULATION_PRICE;
+    } else {
+        event->outcome = OUTCOME_OK;
+        result = ENGINE_OK;
+    }
 
-  // Next, the EE will ensure that the conditions are sufficient for the event to take place.  It will do so by checking for the following errors:
-//a.  The local field is set to true but the affected drone is not local. The outcome will be set to -1 if this error is detected.
-//b.  The affected ship contains insufficient resources of the indicated state (identical resource, locked, and installed values) to accommodate a depletion event. This happens when the newly computed new_quantity field is less than zero. The outcome will be set to -2 if this error is detected.
-//c.  There affected ship contains insufficient cargo space to accommodate the accumulation event. This happens when the recently computed new_cargo value is greater than the cargo capacity of the drone (found in hull table).  The outcome will be set to -3 if this error is detected.
-//d.  The player has insufficient credits to cover the purchase price of transferred items.  The outcome will be set to -4 if this error is detected.
-//e.  The depletion event will violate the affected drone’s self imposed lower limit for the resource. This only applies to cases in which the acting and affected ships are different. The outcome will be set to -5 if this error is detected.
-//f.  The accumulation event will violate the affected drone’s self imposed upper limit for the resource. This only applies to cases in which the acting and affected ships are different. The outcome will be set to -6 if this error is detected.
-//g.  The Depletion Price for items being removed from the cargo bay of a non-allied ship is equal to or greater than the price the player has set on those items. The outcome will be set to -7 if this error is detected.
-//h.  The Accumulation Price for items being added to the cargo bay of a non-allied ship is less than or equal to the price the player has set for those items. The outcome will be set to -8 if this error is detected.
+    // update outcome at DB
+    result = db_update_event(event);
 
-  return result;
+    return result;
 }
 
 /** ****************************************************************************
@@ -184,9 +356,10 @@ ErrorCode_t event_validate_event_conditions(Event_t *event)
 *******************************************************************************/
 ErrorCode_t event_delete_previous_events(Event_t *event) 
 {
-  ErrorCode_t result = ENGINE_OK;
+  ErrorCode_t result = ENGINE_INTERNAL_ERROR;
 
-  // Delete previous events, except this one 
+  if(event)
+    result = db_delete_previous_events(event);
 
   return result;
 }
@@ -202,11 +375,12 @@ ErrorCode_t event_delete_previous_events(Event_t *event)
 *******************************************************************************/
 ErrorCode_t event_abort_action(Event_t *event) 
 {
-  ErrorCode_t result = ENGINE_OK;
+    ErrorCode_t result = ENGINE_INTERNAL_ERROR;
 
-  // Abort action
+    if(event)
+        result = db_abort_action(event->action_id);
 
-  return result;
+    return result;
 }
 
 /** ****************************************************************************
@@ -310,34 +484,6 @@ ErrorCode_t event_process_outcome(Event_t *event)
         // Update observation entries
         result = event_update_observations(event);
     }
-
-#ifdef ENABLED
-  2.  When it finds such an event, the EE will update the resource, credit, and cargo quantities so they represent 
-  total values rather than changes in the total value.  
-  Important: This adjustment will NOT be performed on events associated with market actions.  
-  Depending on the affected quantity, the EE will compute the totals as follows:
-a.  new_quantities. The EE will search for the last processed event with the same drone, resource, installed, and locked values (if such an event exists) and add that quantity to the new_quantities field of the current event.
-b.  new_cargo.  The EE will search for the last processed event with the same drone and add that quantity to the new_cargo field of the current event.
-c.  new_credit.  The EE will search for the last processed event for any drone belonging to the owner of the current drone and add the quantity of the new_credit field of the current event. 
-
-
-3.  Next, the EE will ensure that the conditions are sufficient for the event to take place.  It will do so by checking for the following errors:
-a.  The local field is set to true but the affected drone is not local. The outcome will be set to -1 if this error is detected.
-b.  The affected ship contains insufficient resources of the indicated state (identical resource, locked, and installed values) to accommodate a depletion event. This happens when the newly computed new_quantity field is less than zero. The outcome will be set to -2 if this error is detected.
-c.  There affected ship contains insufficient cargo space to accommodate the accumulation event. This happens when the recently computed new_cargo value is greater than the cargo capacity of the drone (found in hull table).  The outcome will be set to -3 if this error is detected.
-d.  The player has insufficient credits to cover the purchase price of transferred items.  The outcome will be set to -4 if this error is detected.
-e.  The depletion event will violate the affected drone’s self imposed lower limit for the resource. This only applies to cases in which the acting and affected ships are different. The outcome will be set to -5 if this error is detected.
-f.  The accumulation event will violate the affected drone’s self imposed upper limit for the resource. This only applies to cases in which the acting and affected ships are different. The outcome will be set to -6 if this error is detected.
-g.  The Depletion Price for items being removed from the cargo bay of a non-allied ship is equal to or greater than the price the player has set on those items. The outcome will be set to -7 if this error is detected.
-h.  The Accumulation Price for items being added to the cargo bay of a non-allied ship is less than or equal to the price the player has set for those items. The outcome will be set to -8 if this error is detected.
-
-
-4.  If any of these errors are detected, the EE will update the event’s outcome with the indicated value and flag the corresponding action in the action table as aborted. 
-5.  Otherwise, the event’s outcome will be set to 1 and any events (processed or otherwise) with the same event type, drone, resource, installed, and locked values that are older than the game’s data lifespan will be deleted.  This helps ensure that the database size does not grow beyond limits.
-6.  Then, the EE creates a new observations table entry for the acting and affected drones and sets the observation time to that of the event plus the distance in light minutes that the drone is away from the event.  For drones that are local to the event (meaning they are orbiting the same static object), the observation time is identical to the event time.
-7.  If the event is reportable (i.e. the reportable field is true), then the EE creates an observations table entry for the Central Database (Drone ID = 0) and sets the observation time to the event time plus the distance of the drone from the Earth in light minutes.
-8.  If the event is observable and the event does was successfully processed  (i.e. the observable field is true and status = 1), the EE also creates observation table entries for each local drone and sets the observation time to the event time.
-#endif
 
     return result;
 }
