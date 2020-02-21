97be1370ce86d549cfcdca79dbb2ac28bd637ddc
freelancefrivas <freelance.frivas@gmail.com>
2020-02-21 12:08:14 -0300

    Restriction on cargo manifest

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -45,7 +45,6 @@ class AgentController extends Controller
             $query->orderBy($sortParts[0], $sortParts[1]);
         }
         $spaceObjects = $query->paginate($resultsPerPage);
-        // var_dump($spaceObjects);exit;
 
         return response()->json($spaceObjects, 200);
     }
@@ -68,23 +67,11 @@ class AgentController extends Controller
             ->leftJoin('event_types', 'events.event_type', '=', 'event_types.id')
             ->join(DB::raw('(SELECT MAX(events.id) AS id FROM events WHERE drone = ' . $id . '  GROUP BY events.resource ORDER BY id) AS events_latest '), "events_latest.id", '=', 'events.id')//aggregation join
             ->where('new_quantity', ">", 0)
+            ->where('events.outcome', ">", 1)
             ->where('events.drone', '=', $id)->where(function ($q) {
                 return $q->where('event_types.id', EventType::TYPE_INCREMENT_INVENTORY)->orWhere('event_types.id', EventType::TYPE_DECREMENT_INVENTORY);
             })/*->groupBy('resources.name')*/->orderBy('new_quantity', 'desc')->get();
 
-        /*    $agentEvents = DB::table('observations')->select('observations.drone', 'events.timestamp', 'agents.name as agent_name', 'event_types.name as effect', 'resources.id as resource_id', 'resources.name as resource_name',
-                'events.locked', 'agents.agent_id', 'protocols.name as action', 'new_quantity', 'new_credits', 'new_location', 'objects.object_id', 'objects.object_name')
-                ->leftJoin('events', 'observations.event', '=', 'events.id')
-                ->leftJoin('resources', 'events.resource', '=', 'resources.id')
-                ->leftJoin('event_types', 'events.event_type', '=', 'event_types.id')
-                ->leftJoin('actions', 'events.action', '=', 'actions.id')
-                ->leftJoin('protocols', 'actions.protocol', '=', 'protocols.id')
-                ->leftJoin('agents', 'actions.drone', '=', 'agents.agent_id')
-                ->leftJoin('objects', 'events.new_location', '=', 'objects.object_id')
-                ->where('observations.drone', '=', $id)
-                ->orderBy('events.timestamp','desc')
-                ->limit(10)
-                ->get();*/
 
         return view('spacecraft.view', compact('agent', 'cargoManifest'/*, 'agentEvents'*/));
     }
