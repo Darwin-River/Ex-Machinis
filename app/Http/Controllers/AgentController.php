fd9454bd26c5fcee84eb0c0231c3e735ce3b638b
freelancefrivas <freelance.frivas@gmail.com>
2020-02-25 02:21:50 -0300

    Fix for ship's resources display

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -65,12 +65,12 @@ class AgentController extends Controller
             , DB::raw('(events.new_quantity) as new_quantity'), DB::raw('(events.id) as event_id'))
             ->leftJoin('resources', 'events.resource', '=', 'resources.id')
             ->leftJoin('event_types', 'events.event_type', '=', 'event_types.id')
-            ->join(DB::raw('(SELECT MAX(events.id) AS id FROM events  WHERE drone = ' . $id . ' AND new_quantity > 0 AND events.outcome = 1 AND (event_type = ' . EventType::TYPE_INCREMENT_INVENTORY . ' OR event_type = ' . EventType::TYPE_DECREMENT_INVENTORY . ')   GROUP BY events.resource ORDER BY id DESC) AS events_latest '), "events_latest.id", '=', 'events.id')//aggregation join
+            ->join(DB::raw('(SELECT MAX(events.id) AS id FROM events  WHERE drone = ' . $id . ' AND events.outcome = 1 AND (event_type = ' . EventType::TYPE_INCREMENT_INVENTORY . ' OR event_type = ' . EventType::TYPE_DECREMENT_INVENTORY . ')   GROUP BY events.resource ORDER BY id DESC) AS events_latest '), "events_latest.id", '=', 'events.id')//aggregation join
             /*->where('new_quantity', ">", 0)
             ->where('events.outcome', "=", 1)*/
             /*  ->where('events.drone', '=', $id)->where(function ($q) {
                   return $q->where('event_types.id', EventType::TYPE_INCREMENT_INVENTORY)->orWhere('event_types.id', EventType::TYPE_DECREMENT_INVENTORY);
-              })*//*->groupBy('resources.name')*/ ->orderBy('new_quantity', 'desc')->get();
+              })*//*->groupBy('resources.name')*/ ->orderBy('new_quantity', 'desc')->havingRaw('new_quantity > 0')->get();
         $totalWeight = 0;
         foreach ($cargoManifest as $cargoItem) {
             if ($cargoItem->mass && $cargoItem->new_quantity)
