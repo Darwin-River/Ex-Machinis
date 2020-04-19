ce5f9093914d6ac903b1f0b58446b352b2983d8f
Ex Machinis <33500329+darozak@users.noreply.github.com>
2020-04-19 10:39:49 -0400

    Corrects link to player's manual in welcome emails.

--- resources/views/email/welcome_drones_html.blade.php
+++ resources/views/email/welcome_drones_html.blade.php
@@ -2,7 +2,7 @@
 
 <p>You've been assigned three spacecraft ({{$drone1 }}@<?php echo  getenv("MAIL_HOST") ?>, {{$drone2 }}@<?php echo  getenv("MAIL_HOST") ?>, and {{$drone3 }}@<?php echo  getenv("MAIL_HOST") ?>), which you can program and command via email.  The drones will only respond to communications from your email address.</p>
 
-<p>Be sure to check out the <a href="https://<?php echo  getenv("MAIL_HOST") ?>/players-manual">Player's Manual</a> for information on how to interact with your ships.</p>
+<p>Be sure to check out the <a href="https://<?php echo  getenv("MAIL_HOST") ?>/getting-started">Getting Started</a> for information on how to interact with your ships.</p>
 
 <p>Ex Machinis is in the early stages of development.  New features are being added every week and we're doing our best to track down bugs as soon as they appear.  You can help us by logging bugs and potential enhancements on <a href="https://github.com/Darwin-River/Ex-Machinis/issues">GitHub</a>.</p>
 
