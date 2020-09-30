9dfc751203d410a5a554db2c3fbf5438f03597ef
Ex Machinis <33500329+darozak@users.noreply.github.com>
2020-09-30 18:19:12 -0400

    Removed Patreon sponsor request and github links.

--- resources/views/email/welcome_drones_html.blade.php
+++ resources/views/email/welcome_drones_html.blade.php
@@ -3,7 +3,3 @@
 <p>You've been assigned three spacecraft ({{$drone1 }}@<?php echo  getenv("MAIL_HOST") ?>, {{$drone2 }}@<?php echo  getenv("MAIL_HOST") ?>, and {{$drone3 }}@<?php echo  getenv("MAIL_HOST") ?>), which you can program and command via email.  The drones will only respond to communications from your email address.</p>
 
 <p>Be sure to check out the <a href="https://<?php echo  getenv("MAIL_HOST") ?>/getting-started">Getting Started</a> for information on how to interact with your ships.</p>
-
-<p>Ex Machinis is in the early stages of development.  New features are being added every week and we're doing our best to track down bugs as soon as they appear.  You can help us by logging bugs and potential enhancements on <a href="https://github.com/Darwin-River/Ex-Machinis/issues">GitHub</a>.</p>
-
-<p>Although gameplay is entirely free, we do need funds to sustain this project.  Please consider supporting us on <a href="https://www.patreon.com/exmachinis">Patreon</a>.</p>
--- resources/views/email/welcome_drones_text.blade.php
+++ resources/views/email/welcome_drones_text.blade.php
@@ -3,7 +3,3 @@ Welcome to Ex Machinis, a multiplayer space game in which you program remotely p
 You've been assigned three spacecraft ({{$drone1 }}@<?php echo  getenv("MAIL_HOST") ?>, {{$drone2 }}@<?php echo  getenv("MAIL_HOST") ?>, and {{$drone3 }}@<?php echo  getenv("MAIL_HOST") ?>), which you can program and command via email.  The drones will only respond to communications from your email address.
 
 Be sure to check out Getting Started (<?php echo  getenv("MAIL_HOST") ?>/getting-started) for information on how to interact with your ships.
-
-Ex Machinis is in the early stages of development.  New features are being added every week and we're doing our best to track down bugs as soon as they appear.  You can help us by logging bugs and potential enhancements on GitHub (https://github.com/Darwin-River/Ex-Machinis/issues).
-
-Although gameplay is entirely free, we do need funds to sustain this project.  Please consider supporting us on Patreaon (https://www.patreon.com/exmachinis).
